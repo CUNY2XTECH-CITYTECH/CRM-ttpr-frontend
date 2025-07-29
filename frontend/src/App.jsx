867a9eb0bb2c66d08af883b922e1d4716d5c0cf4
maythanhsuaung0-0 <maythanhsuaung1111@gmail.com>
2025-07-29 15:30:43 -0400

    added topbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Header } from "@/components/header";
 import "./App.css";
 import Layout from "./components/layout";
+import { Topbar } from "./components/topbar";
 
 function App() {
   return (
