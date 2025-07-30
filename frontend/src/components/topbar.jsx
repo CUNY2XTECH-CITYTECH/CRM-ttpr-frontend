29e8d192e322f8b6cdbc315cc77e9b4b019e9778
maythanhsuaung0-0 <maythanhsuaung1111@gmail.com>
2025-07-29 22:41:32 -0400

    added 'create companies page'

--- frontend/src/components/topbar.jsx
+++ frontend/src/components/topbar.jsx
@@ -1,26 +1,38 @@
 import React, { useState } from 'react'
 import { Button } from './ui/button'
 
-import { Rows3, LayoutGrid } from 'lucide-react'
-export const Topbar = ({ title,view,setView }) => {
-  const changeView=()=>{
-     setView(!view)
+import {  Rows3, LayoutGrid, ArrowLeft } from 'lucide-react'
+export const Topbar = ({ title, view, setView, mode }) => {
+  const changeView = () => {
+    setView(!view)
   }
   return (
     <div className='grid gap-1 '>
-      <h4 className='py-2 font-semibold uppercase'>{title}</h4>
-      <div className='border-t border-gray-200 py-4 flex gap-1 justify-between'>
-        <Button>Create new</Button>
-        <div className='flex p-1 gap-1 border border-gray-200 rounded-lg'>
-          <Button onclick={changeView} variant={'secondary'} className={'border border-transparent hover:border-gray-300 cursor-pointer'}>
-            <LayoutGrid />
-          </Button>
-    
-          <Button onclick={changeView} variant={'secondary'} className={'border border-transparent hover:border-gray-300 cursor-pointer'}>
-            <Rows3 />
-          </Button>
-        </div>
+      <div className='flex gap-3'>
+      <div className='hover:bg-accent flex gap-1 items-center w-fit px-2 py-1 cursor-pointer border-gray-200  border rounded-sm '>
+        <ArrowLeft className='w-5 h-5'/>
+        <span className='text-sm'>Back</span>
       </div>
+      <h4 className='py-2 font-semibold uppercase'>{title}</h4>
+      </div>
+      {mode === 'read' ?
+        <div className='border-t border-gray-200 py-4 flex gap-1 justify-between'>
+          <Button>Create new</Button>
+          <div className='flex p-1 gap-1 border border-gray-200 rounded-lg'>
+            <Button onClick={changeView} variant={'secondary'} className={'border border-transparent hover:border-gray-300 cursor-pointer'}>
+              <LayoutGrid />
+            </Button>
+
+            <Button onClick={changeView} variant={'secondary'} className={'border border-transparent hover:border-gray-300 cursor-pointer'}>
+              <Rows3 />
+            </Button>
+          </div>
+        </div>
+        : mode === 'create' ?
+          <div className='flex justify-items-end'>
+            <Button className={'ml-auto'}>Save</Button>
+          </div>
+          : <></>}
     </div>
   )
 }
---
+++ frontend/src/pages/admin/create-companies.jsx
@@ -0,0 +1,201 @@
+import Layout from '@/components/layout';
+import { Topbar } from '@/components/topbar';
+import { FormField, FormDescription, FormControl, FormLabel, FormItem, FormMessage, Form } from '@/components/ui/form';
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { Input } from '@/components/ui/input';
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { validateCompaniesForm } from '@/lib/validations';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Textarea } from '@/components/ui/textarea';
+const CreateCompanies = () => {
+  const [view, setView] = useState('row')
+  const companiesForm = useForm({
+    resolver: yupResolver(validateCompaniesForm),
+    defaultValues: {
+      name: '', email: '', mission: '',
+      industry: '',
+      website: '',
+      city: '',
+      street: '',
+      zipcode: ''
+    }
+  })
+  const onSubmit = () => {
+
+  }
+  return (
+    <Layout>
+      <Topbar view={view} setView={setView} title="Add New Company" mode="create" />
+      <div className='w-[80%] m-auto py-4'>
+        <Form {...companiesForm}>
+          <form onSubmit={companiesForm.handleSubmit(onSubmit)} className='space-y-6'>
+            <div className='grid grid-cols-2 gap-2'>
+              <FormField
+                control={companiesForm.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Company Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Company Name" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is Company&apos;s name
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              >
+              </FormField>
+              <FormField
+                control={companiesForm.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Company Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Company Email" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is Company&apos;s Email
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              >
+              </FormField>
+            </div>
+            <div>
+              <FormField
+                control={companiesForm.control}
+                name="mission"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Company Mission</FormLabel>
+                    <FormControl>
+                      <Textarea placeholder="Company Mission" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is Company&apos;s Mission
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              >
+              </FormField>
+
+
+            </div>
+            <div>
+              <FormField
+                control={companiesForm.control}
+                name="industry"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Choose Industry</FormLabel>
+                    <FormControl>
+                      <Select>
+                        <SelectTrigger className="w-full">
+                          <SelectValue placeholder="Theme" {...field}/>
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="light" >Light</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormDescription>
+                      This is Company&apos;s Industry</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              >
+              </FormField>
+            </div>
+            <div>
+              <FormField
+                control={companiesForm.control}
+                name="website"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Company Website URL</FormLabel>
+                    <FormControl>
+                      <Input placeholder="https://www.example.com" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is Company&apos;s Website URL
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              >
+              </FormField>
+            </div>
+            <div className='grid grid-cols-3 gap-2'>
+              <FormField
+                control={companiesForm.control}
+                name="city"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Choose City</FormLabel>
+                    <FormControl>
+                      <Select>
+                        <SelectTrigger className="w-full">
+                          <SelectValue placeholder="Theme" {...field}/>
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="light" >Light</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              >
+              </FormField>
+              <FormField
+                control={companiesForm.control}
+                name="street"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Street</FormLabel>
+                    <FormControl>
+                      <Input placeholder="xxx street" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              >
+              </FormField>
+              <FormField
+                control={companiesForm.control}
+                name="zip"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Zipcode</FormLabel>
+                    <FormControl>
+                      <Input placeholder="xxxxxx-xxxx" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              >
+              </FormField>
+              <FormDescription>
+                This is Company&apos;s Location</FormDescription>
+
+            </div>
+          </form>
+        </Form>
+      </div>
+    </Layout >
+  )
+}
+export default CreateCompanies;
