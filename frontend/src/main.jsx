bd3ca14e05402f6438691bf48632e083b57bb099
shiu.wong <shiuwong1122@gmail.com>
2025-07-30 13:11:01 -0400

    updates

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import LoginPage from './pages/students/signIn'
+import AdminLogin from './pages/admin/signIn'
+import AdminRegister from './pages/admin/register'
+import StudentRegister from './pages/students/register'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,10 +14,13 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/signIn" element={<LoginPage />}/>
+        <Route path="/admin-signIn" element={<AdminLogin />}/>
+        <Route path="/admin-register" element={<AdminRegister />}/>
+        <Route path="/student-register" element={<StudentRegister />}/>
       // add your pages here
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
 
 
