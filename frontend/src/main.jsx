c9458031f914ce45256cf34faab47b045f680ab9
Jared Watson <bluecanna2112@gmail.com>
2025-08-04 13:43:51 -0400

    added profile

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import LoginPage from './pages/students/signIn'
 import CreateCompanies from './pages/admin/create-companies'
 import UpdateCompanies from './pages/admin/update-companies'
+import StudentProfileUpdate from './pages/students/profile'
+import AdminProfileUpdate from './pages/admin/profile'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -15,6 +17,8 @@ createRoot(document.getElementById('root')).render(
         <Route path="/signIn" element={<LoginPage />} />
         <Route path='/admin/create-companies' element={<CreateCompanies />} />
         <Route path='/admin/update-companies' element={<UpdateCompanies/>} />
+        <Route path='/students/profile' element={<StudentProfileUpdate/>}/>
+        <Route path='/admin/profile' element ={<AdminProfileUpdate/>}/>
       // add your pages here
       </Routes>
     </BrowserRouter>
